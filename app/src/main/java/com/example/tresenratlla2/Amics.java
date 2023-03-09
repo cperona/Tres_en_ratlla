6a4abf8833a11a83de46afeccc8215489013cd6e
Christian Perona <cperona@jaumebalmes.net>
2023-03-09 23:28:50 +0100

    Commit final

---
+++ app/src/main/java/com/example/tresenratlla2/Amics.java
@@ -0,0 +1,13 @@
+package com.example.tresenratlla2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Amics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amics);
+    }
+}
--- app/src/main/java/com/example/tresenratlla2/Inici.java
+++ app/src/main/java/com/example/tresenratlla2/Inici.java
@@ -1,8 +1,10 @@
 package com.example.tresenratlla2;
 
 import android.content.Intent;
+import android.media.Image;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -21,6 +23,51 @@ public class Inici extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), Joc.class);
+                startActivity(intent);
+            }
+        });
+
+        Button jugarAmbLaMaquinaButton = findViewById(R.id.jugarAmbLaMaquinaButton);
+        jugarAmbLaMaquinaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Joc.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView configuracioButton = findViewById(R.id.configuracioButton);
+        configuracioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Configuracio.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView perfilButton = findViewById(R.id.perfilButton);
+        perfilButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Perfil.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView rankingButton = findViewById(R.id.rankingButton);
+        rankingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Ranking.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView amicsButton = findViewById(R.id.amicsButton);
+        amicsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Amics.class);
                 startActivity(intent);
             }
         });
